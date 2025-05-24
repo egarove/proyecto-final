7742024b64c68e5f7f84ff618b581d3481b3aaec
egarove <eloygarov@gmail.com>
2025-05-24 23:18:17 +0200

    guardamos alumnos en un ArrayList

--- gestion-alumnos/src/main/java/com/mycompany/gestion/alumnos/GestionAlumnos.java
+++ gestion-alumnos/src/main/java/com/mycompany/gestion/alumnos/GestionAlumnos.java
@@ -48,7 +48,25 @@ public class GestionAlumnos {
                 //recorremos las filas devueltas por la db
                 while(resultado.next()){
                 
+                    int id= resultado.getInt("id");
+                    String name= resultado.getString("nombre");
+                    String surname= resultado.getString("apellidos");
+                    char studentClass= resultado.getString("clase").charAt(0);
+                    String dni= resultado.getString("dni");
+                    int age= resultado.getInt("edad");
+                    char gender= resultado.getString("genero").charAt(0);
+                    String phoneNumber= resultado.getString("telefono");
+                    Date entryDate= resultado.getDate("fecha_ingreso");
+                    double score1= resultado.getDouble("nota1trim");
+                    double score2= resultado.getDouble("nota2trim");
+                    double score3= resultado.getDouble("nota3trim");
+                    
+                    System.out.println(score1);
+                    
+                    Student alumno = new Student (id,name,surname,studentClass,
+                    dni,age,gender,phoneNumber,entryDate,score1,score2,score3);
                 
+                    myStudents.add(alumno);
                 
                 }
         

6b10e13665cd590030964ce1a97fe46f0beab2b5
egarove <eloygarov@gmail.com>
2025-05-24 22:13:21 +0200

    añado gitignore y proyecto

---
+++ gestion-alumnos/src/main/java/com/mycompany/gestion/alumnos/GestionAlumnos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion.alumnos;
+
+/**
+ *
+ * @author eloy/sebastian
+ */
+public class GestionAlumnos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

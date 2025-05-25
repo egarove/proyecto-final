948fa9af630f28f068a61a8ef319ed62a8b04e02
Sebasugami <sebastianrodriguezrodriguez06@gmail.com>
2025-05-25 04:08:39 +0200

    Optimización de código
    Ya no se trabaja alrededor del arraylist, sino en la base de datos (Excepto el manage Student)
    
    - Mejorado el sistema de detectar si un estudiante existe para que solo sea necesario la base de datos
    
    - Mejorado el addStudent y deleteStudent para solo necesitar la conexión
    
    Mejorado el funcionamiento del menú para que esté separado el error por diferencia de tipo y por número no válido
    
    - Limpieza del main para un código más limpio, ahora los Statements y ResulSets están en los respectivos métodos
    
    - Convertidos los try-catch a try with resources para no cerrar manualmente

--- gestion-alumnos/src/main/java/com/mycompany/gestion/alumnos/GestionAlumnos.java
+++ gestion-alumnos/src/main/java/com/mycompany/gestion/alumnos/GestionAlumnos.java
@@ -42,56 +42,27 @@ public class GestionAlumnos {
             //si ha salido bien (NO SALTA AL CATCH)
             System.out.println("ABLE TO CONNECT DATABASE");
 
-            //crear un Statement y ejecutar una consulta
-            Statement consulta = (Statement) conexion.createStatement();
-
-            //obtenemos el resultado de la consulta
-            ResultSet resultado = consulta.executeQuery("SELECT * FROM alumnos");
-
-            //recorremos las filas devueltas por la db
-            while (resultado.next()) {
-
-                int id = resultado.getInt("id");
-                String name = resultado.getString("nombre");
-                String surname = resultado.getString("apellidos");
-                char studentClass = resultado.getString("clase").charAt(0);
-                String dni = resultado.getString("dni");
-                int age = resultado.getInt("edad");
-                String gender = resultado.getString("genero");
-                String phoneNumber = resultado.getString("telefono");
-                double score1 = resultado.getDouble("nota1trim");
-                double score2 = resultado.getDouble("nota2trim");
-                double score3 = resultado.getDouble("nota3trim");
-
-                System.out.println(score1);
-
-                Student alumno = new Student(id, name, surname, studentClass,
-                        dni, age, gender, phoneNumber, score1, score2, score3);
-
-                //añadimos el alumno a la coleccion
-                myStudents.add(alumno);
+            //Menu inicial
+            boolean endProgram = false;
+            while (!endProgram) {
+                switch (menu()) {
+                    case 1:
+                        manageStudents(conexion);
+                        break;
+                    case 2:
+                        addStudent(conexion);
+                        break;
+                    case 3:
+                        deleteStudent(conexion);
+                        break;
+                    case 4:
+                        System.out.println("Closing program");
+                        endProgram = true;
+                    default:
+                        System.out.println("Invalid option. please, introduce a valid one");
+                }
             }
-            //Cerramos los recursos        
-            resultado.close();
-            consulta.close();
-
-            //MOSTRAR MENU
-            int choice = 0;
-            while (choice < 1 || choice > 3) {
-                choice = menu();
-            }
-            switch (choice) {
-                case 1:
-                    listStudents(myStudents);
-                    break;
-                case 2:
-                    addStudent(conexion, myStudents);
-                    break;
-                case 3:
-                    deleteStudent(conexion, myStudents);
-                    break;
-            }
-
+          
             //Cerramos recursos
             conexion.close();
 
@@ -105,16 +76,16 @@ public class GestionAlumnos {
     public static int menu() {
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("1. Gestionar Alumnos");
-        System.out.println("2. Añadir alumno");
-        System.out.println("3. Eliminar alumno");
-        System.out.println("4. Salir");
+        System.out.println("1. Manage students");
+        System.out.println("2. Add student");
+        System.out.println("3. Delete student");
+        System.out.println("4. Exit");
 
         try {
             int choice = keyboard.nextInt();
             return choice;
         } catch (InputMismatchException e) {
-            System.out.println("Error... Introduce 1, 2 or 3!");
+            System.out.println("Invalid input. Please, introduce a number");
             return 0;
         }
     }
@@ -127,9 +98,9 @@ public class GestionAlumnos {
         System.out.println("---END OF THE LIST---");
     }
 
-    public static void addStudent(Connection conexion, ArrayList<Student> myStudents) {
+    public static void addStudent(Connection conexion) {
         Scanner keyboard = new Scanner(System.in);
-        try { 
+        try {
             System.out.println("\n---ADD A NEW STUDENT---");
             System.out.print("Introduce id: ");
             int id = keyboard.nextInt();
@@ -154,7 +125,7 @@ public class GestionAlumnos {
             System.out.print("Introduce third score: ");
             int score3 = keyboard.nextInt();
 
-            if (studentExists(id, myStudents)) {
+            if (studentExists(id, conexion)) {
                 System.out.println("this id already exists!");
             } else {
                 Student alumno = new Student(id, name, surname, studentClass, dni, age, gender, phone, score1, score2, score3);
@@ -178,20 +149,17 @@ public class GestionAlumnos {
         }
     }
 
-    public static void deleteStudent(Connection conexion, ArrayList<Student> myStudents) {
+    public static void deleteStudent(Connection conexion) {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Introduce id to remove: ");
         int idRemove = keyboard.nextInt();
 
         //comprobamos si existe
-        if (studentExists(idRemove, myStudents)) {
-            try {
-                Statement consulta = conexion.createStatement();
+        if (studentExists(idRemove, conexion)) {
+            try (Statement consulta = conexion.createStatement()){
                 consulta.execute("DELETE FROM alumnos WHERE id = " + idRemove + ";");
-                myStudents.removeIf(student -> student.getId() == idRemove);
                 System.out.println("student " + idRemove + " deleted.");
-                //cerramos recursos
-                consulta.close();
+                
             } catch (SQLException ex) {
                 System.out.println(ex.getMessage());
             }
@@ -200,19 +168,25 @@ public class GestionAlumnos {
         }
     }
 
-    public static boolean studentExists(int idRemove, ArrayList<Student> myStudents) {
-        for (Student myStudent : myStudents) {
-            if (myStudent.getId() == idRemove) {
-                return true;
+    public static boolean studentExists(int id, Connection conexion) {
+        try (Statement consulta = (Statement) conexion.createStatement(); ResultSet resultado = consulta.executeQuery("SELECT id FROM alumnos");) {
+
+            while (resultado.next()) {
+                if (id == resultado.getInt("id")) {
+                    return true;
+                }
             }
+        } catch (SQLException ex) {
+            System.out.println("Something went wrong while checking if the student exist");
         }
         return false;
     }
 
-    public static void manageStudents(Statement consulta) {
-        ArrayList<Student> students = new ArrayList<>();
-        try {
-            ResultSet resultado = consulta.executeQuery("SELECT * FROM alumnos");
+    public static void manageStudents(Connection conexion) {
+        ArrayList<Student> students = new ArrayList<>(); //en este arrayList se recopilan todos los alumnos para usar sus datos
+
+        //recopilamos todos los alumnos de la base de datos y los añadimos al arraylist students
+        try (Statement consulta = (Statement) conexion.createStatement(); ResultSet resultado = consulta.executeQuery("SELECT * FROM alumnos");) {
 
             while (resultado.next()) {
 
@@ -230,17 +204,17 @@ public class GestionAlumnos {
 
                 System.out.println(score1);
 
-                Student alumno = new Student(id, name, surname, studentClass,
+                Student student = new Student(id, name, surname, studentClass,
                         dni, age, gender, phoneNumber, score1, score2, score3);
 
-                students.add(alumno);
+                students.add(student);
             }
+
         } catch (SQLException ex) {
             System.out.println("Something went wrong while extracting students from database. " + ex.getMessage());
         } catch (StudentCreationException ex) {
             System.out.println("something went wrong while creating each student");
         }
-        
-        
+
     }
 }
